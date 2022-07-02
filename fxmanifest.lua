7f48b874df2e6060c2b32cd0250e9beaf95bdf5e
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-07-02 16:07:50 +0530

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Loading screen made for Indian Ultra RP'
+author 'Cadburry#7547'
+
+files {
+    'ui.html',    
+    'assets/*',
+}
+
+loadscreen {
+    'ui.html',
+}
+
+loadscreen_cursor 'yes'
